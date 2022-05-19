9cc1a2831b3e0488c6d4f21556dc86392674e72a
init63 <shandy4wd@mail.ru>
2022-05-19 20:30:18 +0400

    upd: lite changes

--- client/src/store/reducers/modal/action-creators.js
+++ client/src/store/reducers/modal/action-creators.js
@@ -1,4 +1,5 @@
 export const ModalActionCreators = {
+  setImageId: (id) => ({type: 'SET_IMAGE_ID', payload: id}),
   setFileName: (fileName) => ({type: 'SET_FILE_NAME', payload: fileName}),
   setModalType: (modalType) => ({type: 'SET_MODAL_TYPE', payload: modalType}),
   setIsActive: (payload) => ({type: 'SET_IS_ACTIVE', payload}),
@@ -11,9 +12,15 @@ export const ModalActionCreators = {
     dispatch(ModalActionCreators.setModalType('SHOW_FULL_IMAGE'));
     dispatch(ModalActionCreators.setIsActive(true));
   },
+  editDescription: (id) => (dispatch) => {
+    dispatch(ModalActionCreators.setImageId(id));
+    dispatch(ModalActionCreators.setModalType('EDIT_DESCRIPTION'));
+    dispatch(ModalActionCreators.setIsActive(true));
+  },
   disable: () => (dispatch) => {
+    dispatch(ModalActionCreators.setImageId(null));
     dispatch(ModalActionCreators.setFileName(''));
     dispatch(ModalActionCreators.setModalType(''));
     dispatch(ModalActionCreators.setIsActive(false));
-  }
+  },
 };
